52468a96fb2f32abb259404dee32369b4e8fb05b
EryTstn <debraktastan@gmail.com>
2025-11-24 13:14:13 +0300

    İnitial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ciftci_Projesi_1"
+include(":app")
+ 
